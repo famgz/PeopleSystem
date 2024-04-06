e1d8c9dd1ef9c99f88bece51a9bd644d7d85d2f3
famgz <famgz@proton.me>
2024-04-06 14:57:47 -0300

    fix: new Cargo tech enums

--- src/com/famgz/composicoes/Cargo.java
+++ src/com/famgz/composicoes/Cargo.java
@@ -1,15 +1,10 @@
 package com.famgz.composicoes;
 
 public enum Cargo {
-    GERENTE,
-    ANALISTA,
-    SUPERVISOR,
-    COORDENADOR,
-    ASSISTENTE,
-    DIRETOR,
     ESTAGIARIO,
-    CONSULTOR,
-    ADMINISTRADOR,
-    TECNICO,
-    OUTRO
+    TRAINEE,
+    JUNIOR,
+    PLENO,
+    SENIOR,
+    SPEC
 }
