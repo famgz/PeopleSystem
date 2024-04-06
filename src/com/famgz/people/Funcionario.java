44db07a4f52fcaab1b402455f43028a227fce8ef
famgz <famgz@proton.me>
2024-04-06 13:54:15 -0300

    feat: Funcionario initial methods implementation

--- src/com/famgz/people/Funcionario.java
+++ src/com/famgz/people/Funcionario.java
@@ -11,19 +11,30 @@ public class Funcionario extends Pessoa {
 	private double salario;
 	private LocalDate dataAdmissao;
 
-	public Funcionario(String nome, LocalDate dataNascimento, Endereco endereco, Telefone telefone, Cargo cargo,
-			double salario, LocalDate dataAdmissao) {
+	public Funcionario(
+			String nome,
+			LocalDate dataNascimento,
+			Endereco endereco,
+			Telefone telefone,
+			Cargo cargo,
+			double salario,
+			LocalDate dataAdmissao) {
 		super(nome, dataNascimento, endereco, telefone);
 		this.cargo = cargo;
 		this.salario = salario;
 		this.dataAdmissao = dataAdmissao;
 	}
 
-	public boolean reajustarSalario() {
+	public boolean reajustarSalario(double novoSalario) {
+		if (novoSalario <= 0) {
+			return false;
+		}
+		this.salario = novoSalario;
 		return true;
 	}
 
-	public boolean promover() {
+	public boolean promover(Cargo novoCargo) {
+		this.cargo = novoCargo;
 		return true;
 	}
 
